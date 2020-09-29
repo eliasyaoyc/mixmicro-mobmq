6a050124c944625441be9f2df1c8979bc46fc0d5
eliasyaoyc <siran0611@gmail.com>
2020-09-29 16:32:15 +0800

    add rpc interface

---
+++ core/src/main/java/xyz/vopen/framework/scheduler/core/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * {@link package-info}
+ *
+ * @author <a href="mailto:siran0611@gmail.com">Elias.Yao</a>
+ * @version ${project.version} - 2020/9/29
+ */
+package xyz.vopen.framework.scheduler.core;
---
+++ core/src/main/java/xyz/vopen/framework/scheduler/core/rpc/RpcGateway.java
@@ -0,0 +1,10 @@
+package xyz.vopen.framework.scheduler.core.rpc;
+
+/**
+ * {@link RpcGateway}
+ *
+ * @author <a href="mailto:siran0611@gmail.com">Elias.Yao</a>
+ * @version ${project.version} - 2020/9/29
+ */
+public interface RpcGateway {
+}
